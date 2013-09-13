81dbfb2b0348b3a7020a74ddec64b5aee17dc567
huxiangtao <elliot130@163.com>
2013-09-13 11:05:10 +0800

    adjust js

--- js/elliot.js
+++ js/elliot.js
@@ -10,13 +10,19 @@
 (function() {
 
     var $text = "返回顶部",
-        $elem = $('<div class="backToTop"></div>').appendTo($('body')).text($text).click(function() {}),
-        $backTopFun = function() {
+        $elem = $('<div class="backToTop">hh</div>');
+
+    $elem.click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 120);
+    });
+
+    function $backTopFun() {
+            $elem.appendTo($('body')).text($text);
             var st = $(document).scrollTop(),
                 winh = $(window).height();
 
             (st > 0) ? $elem.show() : $elem.hide();
-        };
+        }
 
     $(window).bind("scroll",$backTopFun);
 })();
