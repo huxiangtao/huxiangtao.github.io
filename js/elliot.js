18645ab04547a756e4cae0a5aed10543dd1cc765
huxiangtao <elliot130@163.com>
2014-03-31 14:44:46 +0800

    s

--- js/elliot.js
+++ js/elliot.js
@@ -33,13 +33,13 @@
         // 调用图片延时加载
         lazyload();
         function lazyload() {
-            $(document).on("scroll", function() {
+            $(window).bind("scroll", function() {
                 $("img.J_lazy").lazyload({
                     effect : "fadeIn"
                 });
                 $("img.J_lazy").removeClass("J_lazy");
             });
-            $(document).scroll();
+            $(window).scroll();
         };
     })();
 
